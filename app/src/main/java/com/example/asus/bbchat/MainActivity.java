65a82e642c26e3b72ca754feb1768d6b148a864a
asus <823436154@qq.com>
2018-07-26 11:52:53 +0800

    hgh

---
+++ app/src/main/java/com/example/asus/bbchat/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.asus.bbchat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View v = findViewById(R.id.login_data_layout);
+        v.getBackground().setAlpha(100);
+
+        Button  btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this , MainActivity1.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
+
+
+
